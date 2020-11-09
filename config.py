9a07d7cd45ebc5e0047a517ce87cdddbb06ca842
Billy Ashmall <billy.ashmall@nasa.gov>
2020-11-09 10:49:31 -0600

    path fix

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ KEY = '/etc/letsencrypt/live/ceodev.servirglobal.net/privkey.pem'
 CO_ORIGINS = '*'
 
 EE_ACCOUNT = '945396317946-ofvpdldm70jk29d1d4pccu0dhsvkpn8s@developer.gserviceaccount.com'
-EE_KEY_PATH = r'C:\Users\washmall\Desktop\privatekeyCE.pem'
+EE_KEY_PATH = '/home/openforis/nginx_temp/privatekeyCE.pem'
 
 import logging
 LOGGING_LEVEL = logging.INFO
